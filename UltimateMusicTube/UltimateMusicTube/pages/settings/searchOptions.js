a4034ba56fe39f3bddfece401383d554dff67d7c
Yancho Bashev <i_bashev@abv.bg>
2013-09-02 11:04:19 +0300

    Settings page

---
+++ UltimateMusicTube/UltimateMusicTube/pages/settings/searchOptions.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    "use-strict";
+
+    WinJS.UI.Pages.define("/pages/settings/searchOptions.js", {
+        ready: function(element, options) {
+            var searchNumber = Windows.Storage.ApplicationData.current.roamingSettings.values["search-number"];
+
+            if (defaultsearchNumber === undefined) {
+                defaultsearchNumber = 5;
+            }
+
+            searchNumber.value = defaultsearchNumber;
+
+            save.onclick = function(e) {
+                Windows.Storage.ApplicationData.current.roamingSettings.values["search-number"] = searchNumber.value;
+
+            };
+        }
+    });
+})();
