c3969f6cdb07c8966d3a78b2a607ba1fd10cd3b9
Yancho Bashev <i_bashev@abv.bg>
2013-09-02 10:54:42 +0300

    Settings added.

--- UltimateMusicTube/UltimateMusicTube/pages/home/home.js
+++ UltimateMusicTube/UltimateMusicTube/pages/home/home.js
@@ -5,7 +5,21 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            // TODO: Initialize the page here.
+            var searchNumber = Windows.Storage.ApplicationData.current.roamingSettings.values["search-number"];
+            if (searchNumber === undefined) {
+                searchNumber = 5;
+            }
+            ///
+            //if (options && options.queryText) {
+            //    tagName = options.queryText
+            //}
+
+            //var searchPane = Windows.ApplicationModel.Search.SearchPane.getForCurrentView();
+            //searchPane.onquerysubmitted = function(e) {
+            //    search(e.queryText);
+            //};
+
+            //var dtm = Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView();
         }
     });
 })();
